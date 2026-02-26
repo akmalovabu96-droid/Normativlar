323247486c2f1eb2ade5774deaac1f5c30544fd2
Abuxon <abuakmalov96@gmail.com>
2026-02-26 16:28:42 +0500

    4-chi normativ: Searching va Pagination

--- posting/views.py
+++ posting/views.py
@@ -1,11 +1,32 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Post
 from .forms import PostForm
-
+from django.core.paginator import Paginator
+from django.db.models import Q
 
 def post_list(request):
+
+    query = request.GET.get('q')
+
     posts = Post.objects.all().order_by('-created_at')
-    return render(request, 'list.html', {'posts': posts})
+
+    # Searching
+    if query:
+        posts = posts.filter(
+            Q(title__icontains=query) |
+            Q(content__icontains=query)
+        )
+    # Pagination
+    paginator = Paginator(posts, 5)
+
+    page_number = request.GET.get('page')
+
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'list.html', {
+        'page_obj': page_obj,
+        'query': query
+    })
 
 
 def post_detail(request, pk):
@@ -36,3 +57,6 @@ def post_delete(request, pk):
         post.delete()
         return redirect('post_list')
     return render(request, 'delete.html', {'post': post})
+
+
+
